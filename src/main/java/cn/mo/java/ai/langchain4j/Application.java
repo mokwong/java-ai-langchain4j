b22291b05df3906414c5acfed7bb6bbdd32b456e
mo <mo>
2025-07-01 17:51:11 +0800

    first commit

---
+++ src/main/java/cn/mo/java/ai/langchain4j/Application.java
@@ -0,0 +1,18 @@
+package cn.mo.java.ai.langchain4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mo
+ * @Description 启动类
+ * @createTime 2025年07月01日 14:44
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
