1c0e6dc75f36d5d0449f5cf7f85d8d326c6049b5
mo <mo>
2025-07-15 17:50:52 +0800

    mcp test

---
+++ src/test/java/cn/mo/java/ai/langchain4j/MCPTest.java
@@ -0,0 +1,19 @@
+package cn.mo.java.ai.langchain4j;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author mo
+ * @Description test
+ * @createTime 2025年07月15日 14:36
+ */
+@Slf4j
+public class MCPTest {
+
+    @Test
+    public void test() {
+
+    }
+
+}
